61432cc330abd70483cf8d064ff66a07ae98b1ea
miguel <meqh1992@gmail.com>
2024-08-07 20:06:57 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:22-alpine3.19
+
+# Establece el directorio de trabajo en /app
+WORKDIR /app
+
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const linkForm = reactive({
+  link: undefined,
+  search: undefined,
+  aliases: undefined,
+})
+
+const sendForm = ()=> {
+  console.log(linkForm)
+}
+
+
+const searchable = ref(false)
+</script>
+
+<template>
+
+  <div class=" min-h-screen	flex flex-col ">
+    <div class="my-auto">
+      <div class=""> <h1 class="text-5xl py-9"> Generate your reddit Epub Here</h1></div>
+      <div class="grid md:grid-cols-2">
+        <div class="flex">
+          <img src="~/assets/images/animations/Animated-GIF-Banana.gif" alt="reddit example" class="m-auto">
+        </div>
+        <div class="flex">
+          <UContainer class="my-auto w-full">
+            <UForm :state="linkForm"  @submit="sendForm" >
+
+              <div class="flex flex-col  mt-y">
+                <div class="flex-none mb-9">
+                  <UCheckbox v-model="searchable" name="searchable" label="Use seeker" />
+                </div>
+                <div class="flex-1 min-h-80">
+                  <u-form-group label="link" name="link" class="mb-9" v-if="!searchable">
+                    <UInput v-model="linkForm.link"></UInput>
+                  </u-form-group>
+                  <u-form-group label="Aliases" name="aliases" class="mb-9" v-if="searchable">
+                    <UInput v-model="linkForm.aliases"></UInput>
+                  </u-form-group>
+                  <u-form-group label="Find" name="by" class="mb-9" v-if="searchable">
+                    <UInput v-model="linkForm.search"></UInput>
+                  </u-form-group>
+                </div>
+                <div class="flex-none flex">
+                  <UButton type="submit" class="mx-auto px-14 "  >
+                    Find
+                  </UButton>
+                </div>
+
+              </div>
+            </UForm>
+
+          </UContainer>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
